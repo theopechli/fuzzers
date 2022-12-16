25c0f4976b1b80b7a990bf2083f884616e694e76
Theofilos Pechlivanis <theofilos.pechlivanis@gmail.com>
2022-12-16 18:05:07 +0200

    Add 'clap' crate to parse args

--- rust-fuzzer/src/main.rs
+++ rust-fuzzer/src/main.rs
@@ -1,14 +1,34 @@
+use clap::Parser;
 use sha2::{Digest, Sha256};
-use std::collections::BTreeSet;
-use std::io;
-use std::os::unix::process::ExitStatusExt;
-use std::path::Path;
-use std::process::{Command, ExitStatus};
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::Arc;
-use std::time::{Duration, Instant};
+use std::fs;
+use std::{
+    collections::BTreeSet,
+    fs::File,
+    io,
+    os::unix::process::ExitStatusExt,
+    path::{Path, PathBuf},
+    process::{Command, ExitStatus},
+    sync::atomic::{AtomicUsize, Ordering},
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
 const BATCH_SIZE: usize = 10;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "FUZZ TARGET")]
+    binary: PathBuf,
+    #[arg(short, long)]
+    flag: String,
+    #[arg(short, long)]
+    corpus: PathBuf,
+    #[arg(short, long)]
+    output: PathBuf,
+    #[arg(short, long)]
+    threads: usize,
+}
 
 #[derive(Default)]
 struct Statistics {
@@ -33,11 +53,17 @@ impl Rng {
     }
 }
 
-fn fuzz<P: AsRef<Path>>(filename: P, input: &[u8]) -> io::Result<ExitStatus> {
+fn fuzz<P: AsRef<Path>>(
+    binary: &PathBuf,
+    flag: &String,
+    filename: P,
+    input: &[u8],
+) -> io::Result<ExitStatus> {
     std::fs::write(filename.as_ref(), input)?;
 
-    let runner = Command::new("./objdump")
-        .args(&["-x", filename.as_ref().to_str().unwrap()])
+    let runner = Command::new(binary)
+        .arg(flag)
+        .arg(filename.as_ref().to_str().unwrap())
         .output()?;
 
     Ok(runner.status)
@@ -46,7 +72,10 @@ fn fuzz<P: AsRef<Path>>(filename: P, input: &[u8]) -> io::Result<ExitStatus> {
 fn worker(
     thread_id: usize,
     statistics: Arc<Statistics>,
+    binary: PathBuf,
+    flag: String,
     corpus: Arc<Vec<Vec<u8>>>,
+    output_dir: PathBuf,
 ) -> io::Result<()> {
     let mut rng = Rng::new();
     let filename = format!("tmpinput{}", thread_id);
@@ -63,13 +92,16 @@ fn worker(
                 fuzz_input[sel] = rng.rand() as u8;
             }
 
-            let exit = fuzz(&filename, &fuzz_input)?;
+            let exit = fuzz(&binary, &flag, &filename, &fuzz_input)?;
             if let Some(11) = exit.signal() {
                 let mut hasher = Sha256::new();
                 let input = fuzz_input.clone();
                 hasher.update(&input);
                 let result = hasher.finalize();
-                std::fs::write(format!("./crashes/crash_{:#04x}", result), &input)?;
+                std::fs::write(
+                    format!("{}/crash_{:#04x}", output_dir.display(), result),
+                    &input,
+                )?;
                 statistics.crashes.fetch_add(1, Ordering::SeqCst);
             }
         }
@@ -79,8 +111,49 @@ fn worker(
 }
 
 fn main() -> io::Result<()> {
+    let args = Cli::parse();
+
+    let binary = args.binary;
+    println!("Fuzz target is: '{}'", binary.display());
+    {
+        File::open(&binary)
+            .unwrap_or_else(|err| panic!("Could not open file '{}': {}", binary.display(), err));
+    }
+
+    let flag = &["--", &args.flag].join("");
+    println!("Flag is: '{}'", flag);
+
+    let corpus_dir = args.corpus;
+    println!("Corpus directory is: '{}'", corpus_dir.display());
+    {
+        File::open(&corpus_dir).unwrap_or_else(|err| {
+            panic!(
+                "Could not open corpus directory '{}': {:?}",
+                corpus_dir.display(),
+                err
+            )
+        });
+    }
+
+    let output_dir = args.output;
+    println!("Output directory is: '{}'", output_dir.display());
+    match fs::create_dir_all(&output_dir) {
+        Ok(()) => println!(
+            "Created missing output directory: '{}'",
+            output_dir.display()
+        ),
+        Err(e) => panic!(
+            "Could not create missing output directory '{}': {:?}",
+            output_dir.display(),
+            e
+        ),
+    }
+
+    let thread_count = args.threads;
+    println!("Thread count is: {}", thread_count);
+
     let mut corpus = BTreeSet::new();
-    for filename in std::fs::read_dir("./corpus")? {
+    for filename in std::fs::read_dir(corpus_dir)? {
         let filename = filename?.path();
         corpus.insert(std::fs::read(filename)?);
     }
@@ -90,10 +163,13 @@ fn main() -> io::Result<()> {
 
     let stats = Arc::new(Statistics::default());
 
-    for thread_id in 0..2 {
+    for thread_id in 0..thread_count {
         let stats = stats.clone();
+        let binary = binary.clone();
+        let flag = flag.clone();
         let corpus = corpus.clone();
-        std::thread::spawn(move || worker(thread_id, stats, corpus));
+        let output_dir = output_dir.clone();
+        std::thread::spawn(move || worker(thread_id, stats, binary, flag, corpus, output_dir));
     }
 
     let start = Instant::now();
